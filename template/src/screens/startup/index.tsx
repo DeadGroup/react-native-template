ef913d024cb793e70296d75f93ed40019a7e41c5
Dave <minicatdd7@gmail.com>
2025-05-17 23:51:11 +0700

    feat: update navigation type

--- template/src/screens/startup/index.tsx
+++ template/src/screens/startup/index.tsx
@@ -1,10 +1,15 @@
-import {View, Text} from 'react-native';
-import React, {useEffect} from 'react';
-import {useNavigation} from '@react-navigation/native';
+import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import tw from '@/theme';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '@/navigation/params_list';
+import { ScreenNames } from '@/navigation/name';
+
+type Props = NativeStackNavigationProp<RootStackParamList, ScreenNames.Startup>;
 
 const Startup = () => {
-  const {reset} = useNavigation();
+  const { reset } = useNavigation<Props>();
 
   const delay = () => {
     return new Promise(resolver => setTimeout(resolver, 5000));
@@ -13,11 +18,13 @@ const Startup = () => {
   useEffect(() => {
     const init = async () => {
       await delay();
-      reset({index: 0, routes: [{name: 'Home'}]});
+      reset({
+        index: 0,
+        routes: [{ name: ScreenNames.Home }],
+      });
     };
 
     init();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
