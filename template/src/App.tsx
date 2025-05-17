3b437033e97afd6d2da763027d13b20ee566eb51
Dave <minicatdd7@gmail.com>
2025-05-18 00:42:36 +0700

    feat: add axios interceptor

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import AppNavigation from './navigation';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useDeviceContext } from 'twrnc';
+import tw from './theme';
 
 if (__DEV__) {
   require('./reactotron.config');
 }
 
 const App = () => {
+  useDeviceContext(tw);
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
