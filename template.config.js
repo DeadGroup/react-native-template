baabc73d091362a1537a0b9168c28337bd639d64
Dave <minicatdd7@gmail.com>
2025-08-05 22:09:59 +0700

    chore: update script change package name

--- template.config.js
+++ template.config.js
@@ -8,4 +8,6 @@ module.exports = {
 
   // Directory with the template which will be copied and processed by React Native CLI. Template directory should have package.json with all dependencies specified, including `react-native`.
   templateDir: "./template",
+
+  postInitScript: './scripts/init.js',
 };
